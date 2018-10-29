f71e306b95b75deb9193585540cb44076e2e5a26
dythebs <244271172@qq.com>
2018-10-29 15:08:47 +0800

    53

---
+++ 53.cpp
@@ -0,0 +1,19 @@
+static auto xxx=[]{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ans = INT_MIN;
+        int temp = 0;
+        for(int i = 0 ; i < nums.size() ; i++)
+        {
+            temp += nums[i];
+            ans = max(ans, temp);
+            temp = max(0, temp);
+        }
+        return ans;
+    }
+};
