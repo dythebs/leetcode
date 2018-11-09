b3541f5b1dba24a66e0cde81302bb866d70dedbf
dythebs <244271172@qq.com>
2018-11-09 11:59:39 +0800

    70

---
+++ 70.cpp
@@ -0,0 +1,20 @@
+static auto xxx=[]{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 1)
+            return 1;
+        if(n == 2)
+            return 2;
+        int dp[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3 ; i <= n ; i++)
+            dp[i] = dp[i-1] + dp[i-2];
+        return dp[n];
+    }
+};
