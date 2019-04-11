17a4bd5135a7a44645af0d3d73787de730c9221d
dythebs <244271172@qq.com>
2019-04-11 19:31:39 +0800

    90

---
+++ 90.cpp
@@ -0,0 +1,29 @@
+static auto xxx=[]{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+class Solution {
+    vector<vector<int>> ans;
+    vector<int> temp;
+    void dfs(int deep, vector<int> &n, int start) {
+        if(deep <= n.size())
+            ans.push_back(temp);
+        for(int i = start ; i < n.size() ; i++)
+        {
+            if(i > start && n[i] == n[i-1])
+                continue;
+            temp.push_back(n[i]);
+            dfs(deep+1, n, i+1);
+            temp.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        ans.clear();
+        temp.clear();
+        dfs(0, nums, 0);
+        return ans;
+    }
+};
